154bc727c10e479ea65aed971e7fac5e1f73453a
riccardococciolo <73590570+riccardococciolo@users.noreply.github.com>
2025-01-29 17:03:25 +0100

    first commit

---
+++ crazyZoo_back/src/main/java/com/betacom/cz/CrazyZooBackApplication.java
@@ -0,0 +1,13 @@
+package com.betacom.cz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrazyZooBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrazyZooBackApplication.class, args);
+	}
+
+}
