6c427bcea0ebf28e8a57c94c17b134f2d6aa1a9b
AndreaGiga00 <59709507+AndreaGiga00@users.noreply.github.com>
2025-02-03 16:54:18 +0100

    CarrelloDTO + CarelloReq

--- crazyZoo_back/src/main/java/com/betacom/cz/dto/CarelloDTO.java
+++ crazyZoo_back/src/main/java/com/betacom/cz/dto/CarelloDTO.java
@@ -1,5 +1,20 @@
 package com.betacom.cz.dto;
 
 public class CarelloDTO {
+	
+	
+	private UtenteDTO utente;
+
+	public UtenteDTO getUtente() {
+		return utente;
+	}
+
+	public void setUtente(UtenteDTO utente) {
+		this.utente = utente;
+	}
+
+	
+	
+	//ProdottoDTO prod;
 
 }
