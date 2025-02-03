29325ffc4b600409873d072f66870a58697dd0bc
Maniaf <manuel.iafano76@gmail.com>
2025-02-03 17:10:13 +0100

    Update DTO Recensione e Prodotto
    Update DTO Recensione e Prodotto

--- crazyZoo_back/src/main/java/com/betacom/cz/dto/ProdottoDTO.java
+++ crazyZoo_back/src/main/java/com/betacom/cz/dto/ProdottoDTO.java
@@ -6,6 +6,9 @@ public class ProdottoDTO {
 	private Double prezzo;
 	private Integer quantita;
 	private String titolo;
+	private AnimaleDTO animale;
+	private MarcaDTO marca;
+	
 	public Integer getId() {
 		return id;
 	}
@@ -30,12 +33,27 @@ public class ProdottoDTO {
 	public void setTitolo(String titolo) {
 		this.titolo = titolo;
 	}
-	public ProdottoDTO(Integer id, Double prezzo, Integer quantita, String titolo) {
+	public AnimaleDTO getAnimale() {
+		return animale;
+	}
+	public void setAnimale(AnimaleDTO animale) {
+		this.animale = animale;
+	}
+	public MarcaDTO getMarca() {
+		return marca;
+	}
+	public void setMarca(MarcaDTO marca) {
+		this.marca = marca;
+	}
+	
+	public ProdottoDTO(Integer id, Double prezzo, Integer quantita, String titolo, AnimaleDTO animale, MarcaDTO marca) {
 		super();
 		this.id = id;
 		this.prezzo = prezzo;
 		this.quantita = quantita;
 		this.titolo = titolo;
+		this.animale = animale;
+		this.marca = marca;
 	}
 	public ProdottoDTO() {
 		super();
