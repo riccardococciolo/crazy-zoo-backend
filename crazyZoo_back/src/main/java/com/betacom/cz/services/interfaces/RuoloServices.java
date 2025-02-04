6dac60795430481d6f6a579e64a68c2bda86a264
DarioZancuo <dario.zancuo@hotmail.it>
2025-02-04 16:58:30 +0100

    ruolo

--- crazyZoo_back/src/main/java/com/betacom/cz/services/interfaces/RuoloServices.java
+++ crazyZoo_back/src/main/java/com/betacom/cz/services/interfaces/RuoloServices.java
@@ -1,5 +1,15 @@
 package com.betacom.cz.services.interfaces;
 
-public interface RuoloServices {
+import java.util.List;
+import com.betacom.cz.dto.RuoloDTO;
+import com.betacom.cz.request.RuoloRequest;
 
+public interface RuoloServices {
+	
+	void create(RuoloRequest req) throws Exception;
+	void update(RuoloRequest req) throws Exception;
+	void delete(RuoloRequest req) throws Exception;
+	
+	List<RuoloDTO> listAll();
+	
 }
