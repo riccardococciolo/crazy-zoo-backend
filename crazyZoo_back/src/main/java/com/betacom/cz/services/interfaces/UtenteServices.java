6acccd629927b00bd3e5bee08e2b4a10c644c06a
Giobbs <luc.florio@libero.it>
2025-02-06 17:04:17 +0100

    utenti

--- crazyZoo_back/src/main/java/com/betacom/cz/services/interfaces/UtenteServices.java
+++ crazyZoo_back/src/main/java/com/betacom/cz/services/interfaces/UtenteServices.java
@@ -1,5 +1,16 @@
 package com.betacom.cz.services.interfaces;
 
-public interface UtenteServices {
+import java.util.List;
+
+import com.betacom.cz.dto.UtenteDTO;
+import com.betacom.cz.request.UtenteRequest;
+
+public interface UtenteServices 
+{
+	void create(UtenteRequest req) throws Exception;
+	void delete(UtenteRequest req) throws Exception;
+	
+	List<UtenteDTO> listAll();
+	UtenteDTO listByID(Integer id) throws Exception;
 	
 }
