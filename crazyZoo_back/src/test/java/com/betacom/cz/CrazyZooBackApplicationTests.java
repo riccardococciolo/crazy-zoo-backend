4d61c35bf2e26bf68ed3efd08d1d4253ccf01ccc
DarioZancuo <dario.zancuo@hotmail.it>
2025-02-11 17:38:43 +0100

    conflict fix appTest

--- crazyZoo_back/src/test/java/com/betacom/cz/CrazyZooBackApplicationTests.java
+++ crazyZoo_back/src/test/java/com/betacom/cz/CrazyZooBackApplicationTests.java
@@ -5,9 +5,14 @@ import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 import org.springframework.boot.test.context.SpringBootTest;
 import com.betacom.cz.controller.AnimaliControllerTest;
+import com.betacom.cz.controller.CarrelliControllerTest;
 import com.betacom.cz.controller.ImmagineControllerTest;
 import com.betacom.cz.controller.MarcaControllerTest;
+import com.betacom.cz.controller.OrdineControllerTest;
+import com.betacom.cz.controller.ProdottiCarrelloControllerTest;
 import com.betacom.cz.controller.ProdottoControllerTest;
+import com.betacom.cz.controller.ProdottoOrdineControllerTest;
+import com.betacom.cz.controller.RecensioneControllerTest;
 import com.betacom.cz.controller.TipologiaControllerTest;
 import com.betacom.cz.controller.UtenteControllerTest;
 
@@ -16,12 +21,14 @@ import com.betacom.cz.controller.UtenteControllerTest;
 			  	AnimaliControllerTest.class,
 			  	MarcaControllerTest.class,
 			  	TipologiaControllerTest.class,
-<<<<<<< HEAD
-			  	UtenteControllerTest.class
-=======
 			  	ProdottoControllerTest.class,
-			  	ImmagineControllerTest.class
->>>>>>> Dario/Test_prodotto+immagine
+			  	ImmagineControllerTest.class,
+			  	UtenteControllerTest.class,
+			  	RecensioneControllerTest.class,
+			  	OrdineControllerTest.class,
+			  	CarrelliControllerTest.class,
+			  	ProdottoOrdineControllerTest.class,
+			  	ProdottiCarrelloControllerTest.class
 			  })
 
 @SpringBootTest
