1c711de0fb43a5158c87ec622f0962cfc9893ea5
DarioZancuo <dario.zancuo@hotmail.it>
2025-02-11 10:36:13 +0100

    test suite

--- crazyZoo_back/src/test/java/com/betacom/cz/CrazyZooBackApplicationTests.java
+++ crazyZoo_back/src/test/java/com/betacom/cz/CrazyZooBackApplicationTests.java
@@ -1,7 +1,16 @@
 package com.betacom.cz;
 
 import org.junit.jupiter.api.Test;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
 import org.springframework.boot.test.context.SpringBootTest;
+import com.betacom.cz.controller.AnimaliControllerTest;
+
+@Suite
+@SelectClasses({
+			  	AnimaliControllerTest.class
+			  	
+			  })
 
 @SpringBootTest
 class CrazyZooBackApplicationTests {
