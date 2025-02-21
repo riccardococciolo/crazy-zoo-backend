b8dcdebca83d9fe01f504da948cda41857ed2f57
DarioZancuo <dario.zancuo@hotmail.it>
2025-02-21 12:02:08 +0100

    crossorigin

--- crazyZoo_back/src/main/java/com/betacom/cz/controller/OrdineController.java
+++ crazyZoo_back/src/main/java/com/betacom/cz/controller/OrdineController.java
@@ -2,6 +2,7 @@ package com.betacom.cz.controller;
 
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +17,7 @@ import com.betacom.cz.services.interfaces.OrdineServices;
 
 @RestController
 @RequestMapping("/rest/ordini")
+@CrossOrigin(origins = "${url_api}")
 public class OrdineController {
 	
 	@Autowired
